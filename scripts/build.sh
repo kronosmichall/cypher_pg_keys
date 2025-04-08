f5ee6c91eb4b8af13e1b29deaecd4e4daeaf465b
kronosmichall <kronosmichall@gmail.com>
2025-04-08 10:53:24 +0200

    git ignore

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/bash
 mvn clean package -DskipTests
+sudo rm /var/lib/neo4j/plugins/neo4j-custom-procedure-1.0-SNAPSHOT.jar
+sudo cp target/neo4j-custom-procedure-1.0-SNAPSHOT.jar /var/lib/neo4j/plugins/
+sudo systemctl restart neo4j
